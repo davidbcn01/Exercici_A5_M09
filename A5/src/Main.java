89c51fe910aa9a0e352af5ff37b18cbab06349b9
davidbcn01 <72155658+davidbcn01@users.noreply.github.com>
2020-11-27 17:07:10 +0100

    Add files via upload

---
+++ A5/src/Main.java
@@ -0,0 +1,18 @@
+import java.security.KeyPair;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    KeyPair keys = a5.randomGenerate(1024);
+        System.out.println(keys.getPrivate());
+
+        System.out.println("Introduce el mensaje a cifrar: ");
+    String msg = sc.nextLine();
+
+
+
+
+
+    }
+}
